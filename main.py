3c3696cbfc94f9c34b00060c6dbbd9aa08ccdcd4
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-16 20:49:41 +0000

    snake movement...

--- main.py
+++ main.py
@@ -9,27 +9,17 @@ turtle_list = []
 directions = [0, 90, 180, 270]
 speed = [10, 20, 30, 40, 50]
 
-
-
 for turtle_index in range(3):
-    crazy = Turtle()
+    crazy = Turtle(shape="square")
     crazy.penup()
-    crazy.shape("square")
     crazy.color("white")
     crazy.goto(-(turtle_index * 20), 0)
-    crazy.forward(random.choice(speed))
-    crazy.setheading(random.choice(directions))
     turtle_list.append(crazy)
 
-
-for turtle in turtle_list:
-    turtle.forward(random.choice(speed))
-    turtle.setheading(random.choice(directions))
-
-
-
-
-
-
+is_game_on = True
+while is_game_on:
+    for turtle in turtle_list:
+        turtle.forward(20)
+        turtle.setheading(90)
 
 screen.exitonclick()
